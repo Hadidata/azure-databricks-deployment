fcca838f2a0121ef69617d6a790bef8f34ed1f69
Hadi Ali <ali.hadi071@gmail.com>
2024-01-02 17:41:43 -0700

    upgrading version to most current for providers

--- versions.tf
+++ versions.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.63.0"
+      version = "=3.85.0"
     }
     databricks = {
       source  = "databricks/databricks"
-      version = "=1.5.0"
+      version = "=1.33.0"
     }
 
   }
